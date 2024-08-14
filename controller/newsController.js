6f48b2b326f28041d417ce3da27834120f792ee1
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-08-14 03:01:20 +0300

    Fix build issues

--- controller/newsController.js
+++ controller/newsController.js
@@ -2,12 +2,28 @@ const newsModel = require('../models/newsModel');
 
 const addNews = async function(req, res) {
     try {
-        const { text } = req.body;
-        const newText = new newsModel({
-            text,
+        const {
+            news_title,
+            news_photo,
+            news_day,
+            news_month,
+            news_year,
+            news_link,
+            news_details
+        } = req.body;
+
+        const newNews = new newsModel({
+            news_title,
+            news_photo,
+            news_day,
+            news_month,
+            news_year,
+            news_link,
+            news_details
         });
-        await newText.save();
-        res.status(200).json({ message: 'news sent successfully' });
+
+        await newNews.save();
+        res.status(200).json({ message: 'News added successfully' });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'INTERNAL SERVER ERROR' });
@@ -18,7 +34,7 @@ const getNews = async function(req, res) {
     try {
         const response = await newsModel.find();
         if (response.length === 0) {
-            return res.status(404).json({ message: 'NO DATA' });
+            return res.status(404).json({ message: 'No news found' });
         }
         res.status(200).json(response);
     } catch (error) {
@@ -27,5 +43,4 @@ const getNews = async function(req, res) {
     }
 };
 
-
-module.exports = {addNews, getNews};
+module.exports = { addNews, getNews };
--- models/newsModel.js
+++ models/newsModel.js
@@ -1,7 +1,38 @@
 const mongoose = require('mongoose');
 
 const newsSchema = new mongoose.Schema({
-    text: String,
+  news_title: {
+    type: String,
+    required: true
+  },
+  news_photo: {
+    type: String,
+    required: false
+  },
+  news_day: {
+    type: Number,
+    min: 1,
+    max: 31,
+    required: true
+  },
+  news_month: {
+    type: String,
+    required: true
+  },
+  news_year: {
+    type: Number,
+    min: 1900,
+    max: 2099,
+    required: true
+  },
+  news_link: {
+    type: String,
+    required: true
+  },
+  news_details: {
+    type: String,
+    required: false
+  }
 });
 
 const newsModel = mongoose.model('news', newsSchema);
