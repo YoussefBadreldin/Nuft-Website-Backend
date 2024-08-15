33092901edb6a6d9fd76bf35c5699cb1d7f4c549
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-08-16 02:53:34 +0300

    Fix build issues

---
+++ api/index.js
@@ -0,0 +1,79 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Initialize express app
+const app = express();
+
+// Import routes
+const authRoutes = require('../router/userRouter');
+const news = require('../router/newsRouter');
+const Schoralships = require('../router/SchoralshipsRouter');
+const contact = require('../router/contactRouter');
+const visitorRouter = require('../router/visitorRouter');
+const national = require('../router/nationalRouter');
+const national_Links = require('../router/national_links_router');
+const nationaldorms = require('../router/nationalinfoDormsRouter');
+const nationaltrans = require('../router/nationaltrans_router');
+const nationaladmission = require('../router/nationaladmission');
+const private = require('../router/privateRouter');
+const private_Links = require('../router/private_links_router');
+const privatedorms = require('../router/privateinfoDormsRouter');
+const privatetrans = require('../router/privatetrans_router');
+const privateadmission = require('../router/privateadmission');
+const special = require('../router/specialRouter');
+const special_Links = require('../router/special_links_router');
+const specialdorms = require('../router/specialinfoDormsRouter');
+const specialtrans = require('../router/specialtrans_router');
+const specialadmission = require('../router/specialadmission');
+const international = require('../router/internationalRouter');
+const international_Links = require('../router/international_links_router');
+const internationaldorms = require('../router/internationalinfoDormsRouter');
+const internationaltrans = require('../router/internationaltrans_router');
+const internationaladmission = require('../router/internationaladmission');
+
+app.use(cors());
+
+// Connect to MongoDB using environment variable
+mongoose.connect(process.env.MONGODB_URI, {})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/news', news);
+app.use('/Schoralships', Schoralships);
+app.use('/contact', contact);
+app.use('/nationalfaculty', national);
+app.use('/nationallinks', national_Links);
+app.use('/nationaldorms', nationaldorms);
+app.use('/nationaltrans', nationaltrans);
+app.use('/nationaladmission', nationaladmission);
+app.use('/privatefaculty', private);
+app.use('/privatelinks', private_Links);
+app.use('/privatedorms', privatedorms);
+app.use('/privatetrans', privatetrans);
+app.use('/privateadmission', privateadmission);
+app.use('/specialfaculty', special);
+app.use('/speciallinks', special_Links);
+app.use('/specialdorms', specialdorms);
+app.use('/specialtrans', specialtrans);
+app.use('/specialadmission', specialadmission);
+app.use('/internationalfaculty', international);
+app.use('/internationallinks', international_Links);
+app.use('/internationaldorms', internationaldorms);
+app.use('/internationaltrans', internationaltrans);
+app.use('/internationaladmission', internationaladmission);
+app.use('/api', visitorRouter); // Use the visitorRouter for the /api routes
+
+// CORS OPTIONS preflight handler
+app.options('/auth/signup', cors());
+
+// Export the app for serverless deployment
+module.exports = app;
